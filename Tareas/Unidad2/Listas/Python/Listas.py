964dc51e9f488fc7ba6dcd0a1569d83d8024bd1b
nidiavanessacr <L221050151@pabellon.tecnm.mx>
2023-10-04 09:53:55 -0600

    Listas

---
+++ Tareas/Unidad2/Listas/Python/Listas.py
@@ -0,0 +1,3 @@
+itic3=["Eduardo Flores", "Nidia Vanessa Chávez Rendón", "Sajith Alexandro", 
+"Emmanuel de Jesús Esparza", "Melany Chávez", "Fernanda Pantoja", "Jonathan de Jesús", 
+"Santiago Herrera"]
