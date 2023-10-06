4004895144994a72452017a5feb7e647b7c25203
nidiavanessacr <L221050151@pabellon.tecnm.mx>
2023-10-06 10:14:31 -0600

    ListasR

---
+++ Tareas/Unidad2/Listas/R/Listas.R
@@ -0,0 +1,33 @@
+ITIC <- list("Rainer Riveera", "Juan Prieto", "Vane Cháve<");
+Edades <- list(21,18,19);
+Carreras <- list("ITIC", "Lógistica", "Gestión Empresarial", "Mecatrónica", "Industrial");
+EdadesAlumnos <- list(ITIC, Edades);
+View(EdadesAlumnos)
+ITIC[2];
+EdadesAlumnos[1]
+EdadesAlumnos[[1]]
+EdadesAlumnos[1][1]
+EdadesAlumnos[1][1][1]
+View(Edades)
+> length(Edades)
+length(EdadesAlumnos)
+> length(EdadesAlumnos)
+> Edades[4] <- ("32");
+> Edades <- ("30");
+> Edades[5] <- list(25);
+> Edades <- list(18,19,20,21,30);
+> Edades[5] <- list (25)
+> View(Edades)
+> Edades
+> Edades[4] <- NULL;
+> View(Edades)
+> Edades
+> ITIC[5] <- list ("Mely")
+> View(ITIC)
+> ITIC
+> ITIC[1] <- NULL
+> View(ITIC)
+> ITIC
+> Edades2 <- list(17,20,21);
+> View(Edades2)
+> Edades2
