1025e49d6c1063d255808ba9a6fb619bef87055d
nidiavanessacr <L221050151@pabellon.tecnm.mx>
2023-10-09 10:00:47 -0600

    9 de octubre listas INE

---
+++ Tareas/Unidad2/Listas/R/ListasINE.R
@@ -0,0 +1,12 @@
+> library(readr)
+> localidades2 <- read_csv("Estructura_y_Organizacion_de_Datos_NVCR/Tareas/Unidad2/Listas/R/CSV/localidades2.csv")
+> View(localidades2)
+> library(readr)
+> municipios1 <- read_csv("Estructura_y_Organizacion_de_Datos_NVCR/Tareas/Unidad2/Listas/R/CSV/municipios1.csv")
+> View(municipios1)
+> length(localidades2)
+> lenght(municipios1)
+> municipios1[1] //MOstar elementos
+> localidades2[1] //Mostrar elementos
+> lengths(municipios1) //Contar los elemntos de la lista
+> lengths(localidades2)
