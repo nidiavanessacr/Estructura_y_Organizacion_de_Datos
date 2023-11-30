6ed46c20a25bcd5cc1a1ccf30d69a2e194b5e5cf
nidiavanessacr <L221050151@pabellon.tecnm.mx>
2023-11-30 16:46:29 -0600

    ÁrbolesITPA2

---
+++ Tareas/Unidad3/arbol1.py
@@ -0,0 +1,14 @@
+#árboles binarios
+
+from anytree import Node, RenderTree
+
+root = Node(10)
+
+level_1_child_1 = Node(34, parent=root)
+level_1_child_3 = Node(4, parent=root)
+level_1_child_2 = Node(89, parent=root)
+level_2_child_1 = Node(45, parent=level_1_child_1)
+level_2_child_2 = Node(50, parent=level_1_child_2)
+
+for pre, fill, node in RenderTree(root):
+    print("%s%s" % (pre, node.name))
